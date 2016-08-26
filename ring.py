e5a039ffb0fffbf1a66e3413690d730ff99f8a15
Charlie Burrows <charlie.burrows@gmail.com>
2016-08-26 00:18:37 -0400

    Refactored to procedural style so I can use @jit instead of@jitclass

--- ring.py
+++ ring.py
@@ -1,169 +1,148 @@
-from math import sqrt, sin, cos, radians, pi
+from math import sqrt, sin, cos, radians, pi, isnan
 from random import choice, random, randrange
 import tkinter as tk
 import numpy as np
-from numba import jitclass, int64, float64
+from numba import jit, int64, float64
 import svgwrite
 
-spec = [('npoints', int64),
-        ('points', float64[:,:]),
-        ('array_len', int64),
-        ('insert_counter', int64),
-        ('connections', int64[:,:]),
-        ('repel_dist', float64),
-        ('attr_dist', float64)
-        ]
-@jitclass(spec)
-class SoftRingAnimator():
-    def __init__(self, npoints, array_len):
-        self.points = np.zeros((array_len,2), dtype=np.float64)
-        self.npoints = npoints
-        self.array_len = array_len
-        self.insert_counter = 0
-        self.connections = np.zeros((self.array_len,2), dtype=np.int64)
-        self._ring(5*self.npoints / (2*pi), 300, 300)
-        self.repel_dist = 5
-        self.attr_dist = 3
+#@jit((float64, int64, float64[:,:], int64[:,:]))
+def ring(radius, npoints, points, connections, x, y):
+    for i in range(npoints):
+        a = i * 2 * pi / npoints
+        points[i][0] = radius*cos(a) + x
+        points[i][1] = radius*sin(a) + y
+        connections[i][0] = (i - 1) % npoints
+        connections[i][1] = (i + 1) % npoints
 
-    def _ring(self, radius, xpos, ypos):
-        angles = []
-        for i in range(self.npoints):
-            angles.append(i * 2 * pi / self.npoints)
-        for i,a in enumerate(angles):
-            self.points[i][0] = xpos + radius*cos(a)
-            self.points[i][1] = ypos + radius*sin(a)
-            self.connections[i][0] = (i - 1) % self.npoints
-            self.connections[i][1] = (i + 1) % self.npoints
+#@jit(int64(float64[:,:], int64[:,:]))
+def setup(points, connections):
+    starting_points = []
+    npoints = 0
+
+    starting_points.append(npoints)
+    ring(10, 10, points[npoints:npoints+10], connections[npoints:npoints+10], -10, 0)
+    npoints += 10
+
+    starting_points.append(npoints)
+    ring(10, 10, points[npoints:npoints+10], connections[npoints:npoints+10], 10, 0)
+    connections[npoints:npoints+10] += npoints
+    npoints += 10
+
+    #starting_points.append(npoints)
+    #ring(10, 10, points[npoints:npoints+10], connections[npoints:npoints+10])
+    #npoints += 10
+
+    return npoints, starting_points
+    
+@jit(int64(int64, float64[:,:], int64[:,:]))
+def insert(npoints, points, connections):
+    n1 = randrange(npoints)
+    p1 = points[n1]
+    c1 = connections[n1]
+
+    n2 = c1[0]
+    p2 = points[n2]
+    c2 = connections[n2]
+
+    cnew = connections[npoints]
+    
+    points[npoints] = p1 + (p2 - p1) / 2
+    
+    #print(n2, c2, n1, c1)
+    c1[0] = npoints
+    cnew[1] = n1
+    cnew[0] = n2
+    c2[1] = npoints
+    #print(n2, c2, n1, c1, cnew)
+
+    npoints += 1
+    return npoints
+
+@jit((int64, float64[:,:], int64[:,:], float64, float64))
+def iterate(npoints, points, connections, attr_dist, repel_dist):
+    repel_acc = np.array([0,0], dtype=np.float64)
+    attr_acc = np.array([0,0], dtype=np.float64)
+    for i1 in range(npoints):
+        repel_acc[0] = repel_acc[1] = 0.0
+        attr_acc[0] = attr_acc[1] = 0.0
+        for i2 in range(npoints):
+            if i1 == i2: continue
+            delta = points[i1] - points[i2]
+            d = sqrt(delta[0]*delta[0] + delta[1]*delta[1])
+            if i2 == connections[i1][0] or i2 == connections[i1][1]:
+                # neighbour
+                if d > attr_dist:
+                    attr_acc -= delta / d
+            else: # not neighbour
+                if d < repel_dist:
+                    repel_acc += delta / d
+
+        if repel_acc[0] + repel_acc[1] != 0.0:
+            repel_acc /= np.sqrt(repel_acc[0]*repel_acc[0] + repel_acc[1]*repel_acc[1])
             
-    def insert(self):
-        if self.npoints >= self.array_len:
-            return
+        if attr_acc[0] + attr_acc[1] != 0.0:
+            attr_acc /= np.sqrt(attr_acc[0]*attr_acc[0] + attr_acc[1]*attr_acc[1])
 
-        n1 = randrange(self.npoints)
-        p1 = self.points[n1]
-        c1 = self.connections[n1]
+        points[i1] += 0.5 * (attr_acc + repel_acc)
 
-        n2 = c1[0]
-        p2 = self.points[n2]
-        c2 = self.connections[n2]
+@jit(int64(int64, int64, float64[:,:], int64[:,:]))
+def run(iterations, npoints, points, connections):
+    insert_rate = 1
+    attr_dist = 2.
+    repel_dist = 4.
 
-        cnew = self.connections[self.npoints]
-        
-        self.points[self.npoints] = p1 + (p2 - p1) / 2
-        
-        #print(n2, c2, n1, c1)
-        c1[0] = self.npoints
-        cnew[1] = n1
-        cnew[0] = n2
-        c2[1] = self.npoints
-        #print(n2, c2, n1, c1, cnew)
+    n = insert_rate
+    for _ in range(iterations):
+        n -= 1
+        if n == 0:
+            npoints = insert(npoints, points, connections)
+            n = insert_rate
 
-        self.npoints += 1
+        iterate(npoints, points, connections, attr_dist, repel_dist)
+    return npoints
 
-    def iterate(self):
-        self.insert_counter += 1
-        if self.insert_counter == 1:
-            self.insert()
-            self.insert_counter = 0
-
-        repel_acc = np.array([0,0], dtype=np.float64)
-        attr_acc = np.array([0,0], dtype=np.float64)
-        for i1 in range(self.npoints):
-            repel_acc[0] = repel_acc[1] = 0.0
-            attr_acc[0] = attr_acc[1] = 0.0
-            for i2 in range(self.npoints):
-                if i1 == i2: continue
-                delta = self.points[i1] - self.points[i2]
-                d = sqrt(delta[0]*delta[0] + delta[1]*delta[1])
-                if i2 == self.connections[i1][0] or i2 == self.connections[i1][1]:
-                    # neighbour
-                    if d > self.attr_dist:
-                        attr_acc -= delta / d
-                else: # not neighbour
-                    if d < self.repel_dist:
-                        repel_acc += delta / d
-
-            if repel_acc[0] + repel_acc[1] != 0.0:
-                repel_acc /= np.sqrt(repel_acc[0]*repel_acc[0] + repel_acc[1]*repel_acc[1])
-                
-            if attr_acc[0] + attr_acc[1] != 0.0:
-                attr_acc /= np.sqrt(attr_acc[0]*attr_acc[0] + attr_acc[1]*attr_acc[1])
-                #repel_acc[0] = repel_acc[1] = 0.0
-
-            self.points[i1] += 0.5 * (attr_acc + repel_acc)
-
-
-class UI:
-    def __init__(self, animator):
-
-        self.animator = animator
-        master = tk.Tk()
-
-
-        self.w = tk.Canvas(master, width=600, height=600)
-        self.w.pack()
-
-        self.w.create_line(0, 0, 200, 100)
-        self.w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-
-        self.w.create_rectangle(50, 25, 150, 75, fill="blue")
-        
-        self.w.after(0,lambda: self.update(500))
-
-        tk.mainloop()
-        
-    def update(self, iterations):
-        for _ in range(iterations):
-            self.animator.iterate()
-        self.draw()
-        self.w.after(1, lambda: self.update(iterations))
-
-    def draw(self):
-        self.w.delete(tk.ALL)
-        print(self.animator.npoints)
-        for p in self.animator.points:
-            self.w.create_oval(p[0]-1, p[1]-1,
-                               p[0]+1, p[1]+1, 
-                               fill='black')
-
-
-class SVG:
-    def __init__(self, animator):
-      self.animator = animator
-      self.frames = 0
-        
-    def update(self, iterations):
-        for _ in range(iterations):
-            self.animator.iterate()
-        self.draw()
-
-    def walk(self):
-        visited = []
-        n = 0
+def walk(start, connections):
+    visited = []
+    n = start
+    yield(n)
+    while n not in visited:
+        visited.append(n)
+        n = connections[n][0]
         yield(n)
-        while n not in visited:
-            visited.append(n)
-            n = self.animator.connections[n][0]
-            yield(n)
 
-    def walk_coords(self):
-      return ((self.animator.points[n][0], self.animator.points[n][1]) for n in self.walk())
+def walk_coords(start, points, connections):
+  return ((points[n][0], points[n][1]) for n in walk(start, connections))
 
-    def path(self):
-      return ((('M',) if i == 0 else ()) + p for i,p in enumerate(self.walk_coords()))
+def path(start, points, connections):
+  return ((('M',) if i == 0 else ()) + p for i,p in enumerate(walk_coords(start, points, connections)))
 
-    def draw(self):
-        dwg = svgwrite.Drawing('test%05i.svg'%self.frames, profile='tiny')
-        path = svgwrite.path.Path(self.path())
-        path.push('Z')
-        path.stroke('black', width=1)
-        dwg.add(path)
-        dwg.save()
+def draw(starting_points, points, connections, frame):
+    dwg = svgwrite.Drawing('test%05i.svg'%frame, profile='tiny')
+    dwg.viewbox(minx=-100, miny=-100, width=200, height=200)
+    fills = ['none', 'blue']
+    for start, fill in zip(starting_points, fills):
+        svgpath = svgwrite.path.Path(path(start, points, connections), fill=fill)
+        svgpath.push('Z')
+        svgpath.stroke('black', width=1)
+        dwg.add(svgpath)
+    dwg.save()
+
+def main():
+    array_len = 5000
+    points = np.zeros((array_len,2), dtype=np.float64)
+    connections = np.zeros((array_len,2), dtype=np.int64)
+    npoints, starting_points = setup(points, connections)
+    print(starting_points)
+    print(connections[0:npoints])
+
+    iterations = 500
+    frames = 1
+    for i in range(frames):
+        npoints = run(iterations, npoints, points, connections)
+        draw(starting_points, points, connections, i)
 
 if __name__ == '__main__':
-    #UI(SoftRingAnimator(10, 5000))
-    svg = SVG(SoftRingAnimator(10, 5000))
-    svg.update(1000)
+    main()
 
 
 
